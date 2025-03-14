0551d3808419e6c40c9259803c7e0c0996d932ec
MK4Kard <misha212032@gmail.com>
2025-03-14 10:07:44 +0500

    Update EntEx.kt

--- EntEx.kt
+++ EntEx.kt
@@ -1,8 +1,16 @@
 import EntranceExams.Exam as Exam
+import EntranceExams.Grade as GradeExam
 import EntranceExams.Abturient as AbturientExam
 import EntranceExams.Teacher as TeacherExam
 fun main(){
-    var Petrov = Exam(14, "июля", "истории", "Петров")
+    var Petrov = Exam(14, "истории", "Петров")
     println("Введите количество экзаменов")
     Petrov.SetCount(readln()!!.toInt())
+    Petrov.Info()
+    println("Введите сколько баллов набрал абитуриент")
+    var Ivanov = GradeExam(0, 100, "истории", "Иванов")
+    Ivanov.point = readln()!!.toInt()
+    Ivanov.Aver()
+    Ivanov.Info()
+    Ivanov.Conc()
 }
